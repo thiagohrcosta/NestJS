5d22242bda41abeb10a84bc052214c71061ef54f
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-07-04 11:54:22 -0300

    Adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
 import { PrsimaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
 
 @Module({
-  // imports: [],
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true,
+  })],
   controllers: [CreateAccountController],
   providers: [PrsimaService],
 })
