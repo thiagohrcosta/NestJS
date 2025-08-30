daa0141ca55409e135899d18bac0502cdd90683e
thiagohrcosta <thiagohrcosta86@gmail.com>
2025-08-30 00:46:32 -0300

    Adding JWT authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { AuthenticateController } from "src/controllers/authenticate-controller";
+import { Env } from "src/env";
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true });
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true });
+
+        if (!privateKey) {
+          throw new Error('JWT_PRIVATE_KEY is not defined');
+        }
+        if (!publicKey) {
+          throw new Error('JWT_PUBLIC_KEY is not defined');
+        }
+
+        return {
+          // signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      }
+    }),
+  ],
+  controllers: [AuthenticateController],
+})
+
+export class AuthModule {}
